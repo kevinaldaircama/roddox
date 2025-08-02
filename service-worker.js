ecf5d044ebcb020fb9de92b6474f2fe9efb6d673
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2025-08-02 08:50:23 -0500

    Update service-worker.js
    Signed-off-by: kevinaldaircama <kevinaldaircamachoserna51@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -4,34 +4,7 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/logo.jpg',
-  '/icon-512.png'
-  '/Interbank pago.html'
-  '/RTB.html'
-  '/Yape fake.html'
-  '/bcp pago.html'
-  '/bienvenido.html'
-  '/billetera.html'
-  '/depósito.html'
-  '/cuentastreaming.html'
-  '/bonus.html'
-  '/bloqueo.html'
-  '/desbloqueo.html'
-  '/diamantes.html'
-  '/fake.html'
-  '/liberación.html'
-  '/login.html'
-  '/loginvip.html'
-  '/menu.html'
-  '/pagos.html'
-  '/perfil.html'
-  '/recarga.html'
-  '/registrarvip.html'
-  '/registro.html'
-  '/sistema.html'
-  '/tunbarwha.html'
-  '/yape pago.html'
-  '/transaccionesvip.html'
-  '/transacciones.html'
+
 ];
 
 self.addEventListener('install', event => {
