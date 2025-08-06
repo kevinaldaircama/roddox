d5436ef24f99a3ca72db82caa30e6d58e81d99bd
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2025-08-06 07:33:05 -0500

    Update and rename consulta DNI.html to firebase/config.js
    Signed-off-by: kevinaldaircama <kevinaldaircamachoserna51@gmail.com>

---
+++ firebase/config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDFxkNJCVxwddZFsqPeXSdeYe87ExEdhQk",
+  authDomain: "roddox-27e1b.firebaseapp.com",
+  databaseURL: "https://roddox-27e1b-default-rtdb.firebaseio.com",
+  projectId: "roddox-27e1b",
+  storageBucket: "roddox-27e1b.firebasestorage.app",
+  messagingSenderId: "108259944325",
+  appId: "1:108259944325:web:4ffd0c6e7815822eb1c6ec",
+  measurementId: "G-MBQ5X9PLF8"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
