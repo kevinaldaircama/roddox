5e5acc75bfc2326630d56375c9affbf09b962cfb
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2025-08-16 06:30:01 -0500

    Add files via upload
    Signed-off-by: kevinaldaircama <kevinaldaircamachoserna51@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,37 @@
-const CACHE_NAME = 'roddox-cache-v1';
+const CACHE_NAME = 'kevintech-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/logo.jpg',
-
+  '/icon-192.png',
+  '/icon-512.png'
+  '/Interbank pago.html'
+  '/RTB.html'
+  '/Yape fake.html'
+  '/bcp pago.html'
+  '/bienvenido.html'
+  '/billetera.html'
+  '/depósito.html'
+  '/cuentastreaming.html'
+  '/bonus.html'
+  '/bloqueo.html'
+  '/desbloqueo.html'
+  '/diamantes.html'
+  '/fake.html'
+  '/liberación.html'
+  '/login.html'
+  '/loginvip.html'
+  '/menu.html'
+  '/pagos.html'
+  '/perfil.html'
+  '/recarga.html'
+  '/registrarvip.html'
+  '/registro.html'
+  '/sistema.html'
+  '/tunbarwha.html'
+  '/yape pago.html'
+  '/transaccionesvip.html'
+  '/transacciones.html'
 ];
 
 self.addEventListener('install', event => {
